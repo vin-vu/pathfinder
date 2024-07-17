4dadf71d0c441ff9ea0f0533b19735a462a41d5c
vin-vu <vince.hvu@gmail.com>
2024-07-16 21:36:23 -0700

    Centered buttons with proper spacing in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,27 @@
-import { AppBar, Toolbar, Button } from "@mui/material";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 export default function Navbar() {
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Button color="inherit">SET START NODE</Button>
-          <Button color="inherit">SET TARGET NODE</Button>
-          <Button color="inherit">ADD WALLS</Button>
-          <Button color="inherit">CLEAR BOARD</Button>
-          <Button color="inherit">RUN ALGORITHM</Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              gap: 10,
+              width: "100%",
+            }}
+          >
+            <Button color="inherit">SET START NODE</Button>
+            <Button color="inherit">SET TARGET NODE</Button>
+            <Button color="inherit">ADD WALLS</Button>
+            <Button color="inherit">CLEAR BOARD</Button>
+            <Button color="inherit">RUN ALGORITHM</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
