fbfac9a5b03dec92e9dab5f5c9a341d54cfe7c4e
vin-vu <vince.hvu@gmail.com>
2024-07-19 15:02:07 -0700

    Displaying unstyled maze grid

--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -1,24 +1,40 @@
-import useState from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Grid } from '@mui/material';
 
 interface BoardCell {
   visited: boolean;
 }
 
 type Board = {
-  [key: string] : BoardCell;
-}
+  [key: string]: BoardCell;
+};
 
 export default function Maze() {
   const [board, setBoard] = useState<Board>({});
 
   useEffect(() => {
-    const newBoard : Board = {};
+    const newBoard: Board = {};
     for (let i = 0; i < 15; i += 1) {
       for (let j = 0; j < 30; j += 1) {
-        newBoard[`{i},{j}`] = { visited: false };
+        newBoard[`${i},${j}`] = { visited: false };
       }
     }
     setBoard(newBoard);
   }, []);
+
+  return (
+    <Grid container>
+      {Object.entries(board).map(([key, cell]) => (
+        <Grid key={key} item>
+          <div
+            style={{
+              height: '20px',
+              width: '20px',
+              backgroundColor: cell.visited === true ? 'white' : 'black',
+            }}
+          ></div>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
