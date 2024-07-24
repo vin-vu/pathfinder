d451b7be667d659a26472cc997c5691a8a3c6ad2
vin-vu <vince.hvu@gmail.com>
2024-07-23 17:32:48 -0700

    Hardcoded start and target coordinates

--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -3,6 +3,8 @@ import { Box, Grid } from '@mui/material';
 
 interface BoardCell {
   visited: boolean;
+  startNode: boolean;
+  targetNode: boolean;
 }
 
 type Board = {
@@ -12,32 +14,67 @@ type Board = {
 export default function Maze() {
   const [board, setBoard] = useState<Board>({});
 
+  const rows: number = 15;
+  const columns: number = 30;
+  const startCoordinates: string = '7,6';
+  const targetCoordinates: string = '7,23';
+
   useEffect(() => {
     const newBoard: Board = {};
-    for (let i = 0; i < 15; i += 1) {
-      for (let j = 0; j < 30; j += 1) {
-        newBoard[`${i},${j}`] = { visited: false };
+    for (let i = 0; i < rows; i += 1) {
+      for (let j = 0; j < columns; j += 1) {
+        const coordinates: string = `${i},${j}`;
+        if (coordinates === startCoordinates) {
+          newBoard[coordinates] = {
+            visited: false,
+            startNode: true,
+            targetNode: false,
+          };
+        } else if (coordinates === targetCoordinates) {
+          newBoard[coordinates] = {
+            visited: false,
+            startNode: false,
+            targetNode: true,
+          };
+        } else {
+          newBoard[coordinates] = {
+            visited: false,
+            startNode: false,
+            targetNode: false,
+          };
+        }
       }
     }
     setBoard(newBoard);
   }, []);
+
+  const handleCellBackgroundColor = (cell: BoardCell) => {
+    if (cell.visited) {
+      return 'black';
+    } else if (cell.startNode) {
+      return 'blue';
+    } else if (cell.targetNode) {
+      return 'red';
+    }
+    return 'white';
+  };
 
   return (
     <Grid
       container
       sx={{
         display: 'grid',
-        'grid-template-columns': 'repeat(30, 1fr)',
+        'grid-template-columns': `repeat(${columns}, 1fr)`,
       }}
     >
       {Object.entries(board).map(([key, cell]) => (
         <Grid key={key} item>
           <Box
             sx={{
-              height: '20px',
-              width: '20px',
+              height: '30px',
+              width: '30px',
               border: '1px solid gray',
-              backgroundColor: cell.visited === true ? 'black' : 'white',
+              backgroundColor: handleCellBackgroundColor(cell),
             }}
           ></Box>
         </Grid>
