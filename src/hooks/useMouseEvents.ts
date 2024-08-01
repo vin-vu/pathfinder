731c57b8cca91ba5dc7d98aab64d1f1e606d0e65
vin-vu <vince.hvu@gmail.com>
2024-08-01 16:11:36 -0700

    Completed methods for useMouseEvent hook

--- src/hooks/useMouseEvents.ts
+++ src/hooks/useMouseEvents.ts
@@ -9,24 +9,59 @@ interface UseMouseEventParams {
   setTargetCoordinates: (coordinates: string) => void;
 }
 
-const useMouseEvents = ({cursorMode, addWalls, setStartCoordinates, setTargetCoordinates}: UseMouseEventParams) => {
+export const useMouseEvents = ({
+  cursorMode,
+  addWalls,
+  setStartCoordinates,
+  setTargetCoordinates,
+}: UseMouseEventParams) => {
   const [mouseStatus, setMouseStatus] = useState<MouseStatuses>({
     down: false,
     move: false,
     up: false,
   });
 
-  const handleMouseDown = useCallback((coordinates: string): void => {
-    // setMouseStatus({ ...mouseStatus, down: true });
-    setMouseStatus({down: true, move: false, up: false });
-    if (cursorMode === 'start') {
-      setStartCoordinates(coordinates);
-    } else if (cursorMode === 'target') {
-      setTargetCoordinates(coordinates);
-    } else if (cursorMode === 'walls') {
-      addWalls(coordinates);
-    }
-  }, [cursorMode, addWalls, setStartCoordinates, setTargetCoordinates]);
+  const handleMouseDown = useCallback(
+    (coordinates: string): void => {
+      setMouseStatus({ down: true, move: false, up: false });
+      if (cursorMode === 'start') {
+        setStartCoordinates(coordinates);
+      } else if (cursorMode === 'target') {
+        setTargetCoordinates(coordinates);
+      } else if (cursorMode === 'walls') {
+        addWalls(coordinates);
+      }
+    },
+    [cursorMode, addWalls, setStartCoordinates, setTargetCoordinates]
+  );
 
+  const handleMouseMove = useCallback(
+    (coordinates: string): void => {
+      setMouseStatus({ down: true, move: true, up: false });
+      if (cursorMode === 'walls') {
+        addWalls(coordinates);
+      }
+    },
+    [cursorMode, addWalls]
+  );
 
+  const handleMouseUp = useCallback((): void => {
+    setMouseStatus({ down: false, move: false, up: true });
+  }, []);
+
+  useEffect(() => {
+    const handleMouseOut = (event: MouseEvent) => {
+      if (mouseStatus.down) {
+        setMouseStatus({ down: false, move: false, up: true });
+      }
+    };
+    document.addEventListener('mouseup', handleMouseUp);
+    document.addEventListener('mouseout', handleMouseOut);
+
+    return () => {
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('mouseout', handleMouseOut);
+    };
+  }, [mouseStatus.down, handleMouseUp]);
+  return { handleMouseDown, handleMouseMove, handleMouseUp };
 };
