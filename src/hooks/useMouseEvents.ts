bd6c0af4743792b5364558b42d469eb1426ceb98
vin-vu <vince.hvu@gmail.com>
2024-08-08 16:22:59 -0700

    Add wall functionality working

--- src/hooks/useMouseEvents.ts
+++ src/hooks/useMouseEvents.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { MouseStatuses } from '../App';
 import { CursorModeType } from '../types/CursorTypes';
 
@@ -37,15 +37,14 @@ export const useMouseEvents = ({
 
   const handleMouseMove = useCallback(
     (coordinates: string): void => {
-      setMouseStatus({ down: true, move: true, up: false });
+      setMouseStatus({ ...mouseStatus, move: true });
       if (cursorMode === 'walls' && mouseStatus.down && mouseStatus.move) {
         addWalls(coordinates);
-        console.log('painting: ', mouseStatus)
+        console.log('painting: ', mouseStatus);
       }
       document.addEventListener(
         'mouseup',
         () => {
-          console.log('mouse up');
           setMouseStatus({ down: false, move: false, up: true });
         },
         { once: true }
@@ -54,31 +53,9 @@ export const useMouseEvents = ({
     [cursorMode, addWalls, mouseStatus]
   );
 
-  // const handleMouseLeave = useCallback(() => {
-  //   if (mouseStatus.down) {
-  //     setMouseStatus((prev) => ({ ...prev, move: false }));
-  //   }
-  // }, [mouseStatus.down]);
-
   const handleMouseUp = useCallback((): void => {
     setMouseStatus({ down: false, move: false, up: true });
   }, []);
-
-  // useEffect(() => {
-  //   const handleMouseOut = (event: MouseEvent) => {
-  //     if (mouseStatus.down) {
-  //       setMouseStatus({ down: false, move: false, up: true });
-  //       console.log('mouse out down')
-  //     }
-  //   };
-  //   document.addEventListener('mouseup', handleMouseUp);
-  //   document.addEventListener('mouseout', handleMouseOut);
-
-  //   return () => {
-  //     document.removeEventListener('mouseup', handleMouseUp);
-  //     document.removeEventListener('mouseout', handleMouseOut);
-  //   };
-  // }, [mouseStatus.down, handleMouseUp]);
 
   return {
     handleMouseDown,
