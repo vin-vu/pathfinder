5f51c0a268ffc4ee659cf2def275e1cdbafebcd9
vin-vu <vince.hvu@gmail.com>
2024-10-01 19:04:08 -0700

    Test for rendering of navbar and its buttons

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,34 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
 
-test('demo', () => {
-  expect(true).toBe(true)
-})
+describe('App Component', () => {
+  test('renders Navbar and its buttons', () => {
+    render(<App />);
+
+    const setStartNodeButton: HTMLElement = screen.getByRole('button', {
+      name: /run algorithm/i,
+    });
+    expect(setStartNodeButton).toBeInTheDocument();
+
+    const setTargetNodeButton: HTMLElement = screen.getByRole('button', {
+      name: /set start node/i,
+    });
+    expect(setTargetNodeButton).toBeInTheDocument();
+
+    const setWallsButton: HTMLElement = screen.getByRole('button', {
+      name: /set target node/i,
+    });
+    expect(setWallsButton).toBeInTheDocument();
+
+    const clearBoardButton: HTMLElement = screen.getByRole('button', {
+      name: /clear board/i,
+    });
+    expect(clearBoardButton).toBeInTheDocument();
+
+    const runAlgoButton: HTMLElement = screen.getByRole('button', {
+      name: /run algorithm/i,
+    });
+    expect(runAlgoButton).toBeInTheDocument();
+  });
+});
